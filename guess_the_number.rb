1a0a0fa0cc213fbb8312b4b8d05fcbc83bab7e07
imacrab53 <zeebra901@hotmail.com>
2014-03-19 17:58:54 +0000

    We did stuff to it

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,10 +1,28 @@
 puts "WELCOME TO THE GAME"
-secret_number = rand(6)
-puts secret_number
+secret_number = rand(5)
+puts "Guess the number between 0 and 5"
+text = gets.chomp.to_i
+
+if secret_number > 3
+end
 
 
 
 
+until text == secret_number
+puts "you're dum"
+    text = gets.chomp.to_i
+end
+
+puts "u won"
+
+#3.times do
+  #if secret_number == text
+ # puts "Congratulations, your right"
+  #  puts "you're dum"
+#    text = gets.chomp.to_i
+ # end
+#end
 
 
 
